dfc5f137badc2a66b8e339d1cf4c48fb2d69c69b
Jaison Jain <jainj@satijalab04.nygenome.org>
2020-07-29 11:05:05 -0400

    init

---
+++ prepare_integrated.R
@@ -0,0 +1,157 @@
+#!/usr/bin/env Rscript
+
+# sample cmd
+# ./prepare_integrated.R --files /home/jainj/data/spleen/ed8a4dbbb1554a5e3227d6dfb2368828/cluster_marker_genes.h5ad,/home/jainj/data/spleen/7fd04d1aba61c35843dd2eb6a19d2545/cluster_marker_genes.h5ad --i ed8a4,7fd04 --ensembl /home/jainj/hubmap/mart_export.txt --markers /home/jainj/hubmap/markers.txt --o /home/jainj/hubmap/out  
+# inside downloaded dir...
+# ./prepare_integrated.R --files $(pwd)/data/ed8a4dbbb1554a5e3227d6dfb2368828.h5ad,$(pwd)/data/7fd04d1aba61c35843dd2eb6a19d2545.h5ad --i ed8a4,7fd04 --ensembl $(pwd)/misc/mart_export.txt --markers $(pwd)/misc/markers.txt --o $(pwd)/output  
+
+library(docopt)
+library(Seurat)
+library(SeuratDisk)
+library(Matrix)
+library(cli)
+
+doc <- 
+  "Prepare integrated HuBMAP object.
+
+Usage:
+  prepare_integrated.R [--files <h5ad-files> --i <ids> --ensembl <path-to-ensembl-file> --markers <path-to-markers-file> --o <output-directory> --verbose <verbosity>]
+
+Options:
+  --files <h5ad-files>                Absolute paths to h5ad files to be integrated (unspaced, comma-separated).
+  --i <ids>                           ID names of objects in h5ad files (unspaced, comma-separated).
+  --ensembl <path-to-ensembl-file>    Absolute path to file with ensemble ID dataframe.
+  --markers <path-to-markers-file>    Absolute path to file with cell type markers.
+  --o <output-directory>              Output directory.
+  --verbose <verbosity>               Verbosity [default: TRUE]."
+
+opts <- docopt(doc)
+files <- unlist(strsplit(opts$files,","))
+ids <- unlist(strsplit(opts$i,","))
+out <- opts$o
+markers <- unlist(lapply(FUN=function(x){strsplit(x,",")}, 
+                         X=readLines(opts$markers)))
+v <- as.logical(opts$verbose)
+
+ens_table <- read.table(opts$ensembl,sep = "\t",header = T,stringsAsFactors = F)
+ens_table <- ens_table[!duplicated(ens_table$Gene.stable.ID),]
+rownames(ens_table) <- ens_table$Gene.stable.ID
+
+objs <- list()
+mito.genes <- vector()
+ribo.genes <- vector()
+for(i in 1:length(ids)) {
+  # read file and make seurat
+  id <- ids[i]
+  cli_h1(paste0("Preparing ",id))
+  file.name <- Seurat:::ExtractField(files[[i]],field=1,delim=".h5ad")
+  suppressMessages(suppressWarnings(
+    Convert(paste0(file.name,".h5ad"), dest = "h5seurat", overwrite = TRUE,verbose=v)))
+  objs[[i]] <- suppressMessages(suppressWarnings(
+    LoadH5Seurat(paste0(file.name,".h5seurat"),graphs=F,verbose=v)))
+  # give gene names
+  ens_names <- sapply(rownames(objs[[i]]),Seurat:::ExtractField,1,"\\.")
+  gene_names <- ens_table[ens_names,]$Gene.name
+  gene_names[is.na(gene_names)] <- ens_names[is.na(gene_names)] # if NA, keep ensemble name
+  gene_names <- gsub(pattern = '_', replacement = '-', x = gene_names)
+  take <- !duplicated(gene_names)
+  objs[[i]][["RNA"]] <- CreateAssayObject(data = objs[[i]][["RNA"]][take,])
+  rownames(objs[[i]][['RNA']]@data) <- gene_names[take]
+  # fix meta.features
+  objs[[i]][['RNA']]@meta.features <- data.frame(row.names=rownames(objs[[i]]))
+  # make counts
+  objs[[i]][['RNA']]@counts <- Matrix(exp(objs[[i]][['RNA']]@data)-1, sparse=T)
+  cnts <- objs[[i]][['RNA']]@counts
+  # percent mito
+  mito.genes.1 <- grep(pattern="MT-",x=rownames(objs[[i]]),value=T)
+  percent.mito <- colSums(cnts[mito.genes.1,]) / colSums(cnts)
+  mito.genes <- union(mito.genes, mito.genes.1)
+  ribo.genes <- union(ribo.genes,
+                      union(grep(pattern="RPL",x=rownames(objs[[i]]),value=T),
+                            grep(pattern="RPS",x=rownames(objs[[i]]),value=T)))
+  # subset cells: 200 < nFeature_RNA < 5000, percent_mito < 30%
+  message(paste0("pre-filter: ",length(Cells(objs[[i]])), " cells"))
+  cells <- which(objs[[i]][['nFeature_RNA']] > 200 &
+                   objs[[i]][['nFeature_RNA']] < 5000 &
+                   percent.mito < 0.30)
+  objs[[i]] <- subset(objs[[i]], cells=cells)
+  message(paste0("post-filter: ",length(Cells(objs[[i]])), " cells"))
+}
+for(i in 1:length(ids)) {
+  # run sctransform
+  cli_h1(paste0("SCTransforming ",ids[[i]]))
+  objs[[i]] <- SCTransform(objs[[i]], ncells=5000, verbose=v)
+  # save seurat
+  suppressMessages(suppressWarnings(SaveH5Seurat(object = objs[[i]],filename = paste0(out,"/",ids[[i]],".h5seurat"),overwrite = T,verbose=v)))
+}
+
+# (save all rownames for later,) and REMOVE mito genes and ribo genes from scale data
+orig.rownames <- list()
+for (i in 1:length(ids)) {
+  orig.rownames[[i]] <- rownames(objs[[i]][['SCT']])
+  VariableFeatures(objs[[i]]) <- setdiff(setdiff(VariableFeatures(objs[[i]]), mito.genes),
+                                         ribo.genes)
+  objs[[i]][['SCT']]@scale.data <- objs[[i]][['SCT']]@scale.data[VariableFeatures(objs[[i]]),]
+}
+save(orig.rownames, file = paste0(out,"/orig.rownames.rda"))
+
+# PROCESS objs individually
+for (i in 1:length(ids)) {
+  cli_h1(paste0("Processing ",ids[[i]]," (PCA, UMAP, neighbor-finding, clustering)"))
+  objs[[i]] <- RunPCA(objs[[i]],verbose=v) # based on scale.data
+  objs[[i]] <- RunUMAP(objs[[i]],dims=1:50,verbose=v) # based on PCA
+  objs[[i]] <- FindNeighbors(objs[[i]],dims=1:50,verbose=v) # SNN graph made in PCA space
+  objs[[i]] <- FindClusters(objs[[i]],resolution=1,verbose=v) # based on SNN
+  suppressMessages(suppressWarnings(SaveH5Seurat(object = objs[[i]],filename = paste0(out,"/",ids[[i]],".h5seurat"),overwrite = T,verbose=v)))
+}
+
+# RELOAD objs and ADDBACK misc
+objs <- list()
+load(paste0(out,"/orig.rownames.rda"))
+for (i in 1:length(ids)) {
+  objs[[i]] <- LoadH5Seurat(file = paste0(out,"/",ids[[i]],".h5seurat"),verbose=v)
+  rownames(objs[[i]][['SCT']]@misc$vst.out$model_pars_fit) <- orig.rownames[[i]]
+  colnames(objs[[i]][['SCT']]@misc$vst.out$model_pars_fit) <- c("theta","(Intercept)","log_umi")
+  rownames(objs[[i]][['SCT']]@misc$vst.out$cell_attr) <- Cells(objs[[i]])
+}
+
+# INTEGRATE objs and process
+cli_h1("Finding anchors and integrating")
+feats.all <- SelectIntegrationFeatures(objs, nfeatures = 3000,verbose=v)
+objs <- PrepSCTIntegration(objs, anchor.features = feats.all,verbose=v)
+anchors.all <- FindIntegrationAnchors(objs, normalization.method = "SCT", anchor.features = feats.all,verbose=v)
+integrated.all <- IntegrateData(anchorset = anchors.all, normalization.method = "SCT",verbose=v)
+cli_h1("Processing integrated object (PCA, UMAP, neighbor-finding, clustering)")
+integrated.all <- RunPCA(integrated.all,verbose=v)
+integrated.all <- RunUMAP(integrated.all,dims=1:50,verbose=v)
+integrated.all <- FindNeighbors(integrated.all,dims=1:50,verbose=v)
+integrated.all <- FindClusters(integrated.all,resolution=c(10,5,1.2),verbose=v)
+integrated.all <- AddMetaData(integrated.all,
+                              metadata = unname(sapply(FUN = Seurat:::ExtractField,
+                                                       X = Cells(integrated.all),
+                                                       delim = "_", field = 2)),
+                              col.name = "sample")
+SaveH5Seurat(integrated.all, filename = paste0(out,"/integrated.h5seurat"),overwrite = T,verbose=v)
+
+# Feature plots and resolution plots for annotation
+integrated.all <- LoadH5Seurat(file = paste0(out,"/integrated.h5seurat"))
+cli_h1("Making plots")
+out.plots <- paste0(out,"/plots")
+system(paste0("mkdir ",out.plots))
+DefaultAssay(integrated.all) <- "SCT"
+for (marker in markers) {
+  png(paste0(out.plots,"/feature_",marker,".png"))
+  print(FeaturePlot(integrated.all,marker))
+  dev.off()
+}
+DefaultAssay(integrated.all) <- "integrated"
+for (name in grep(x=names(integrated.all[[]]), pattern='^integrated_snn_res', value=T)) {
+  resolution <- Seurat:::ExtractField(name,field=2,delim="res.")
+  png(paste0(out.plots,"/clusters_res.",resolution,".png"),width=1000)
+  print(DimPlot(integrated.all,group.by=name,label=T))
+  dev.off()
+}
+png(paste0(out.plots,"/BATCH.png"))
+print(DimPlot(integrated.all,group.by='sample',label=T))
+dev.off()
+
---
+++ prune.R
@@ -0,0 +1,206 @@
+#!/usr/bin/env Rscript
+
+# sample cmd
+# ./prune.R --files /home/jainj/data/spleen/ed8a4dbbb1554a5e3227d6dfb2368828/cluster_marker_genes.h5ad,/home/jainj/data/spleen/7fd04d1aba61c35843dd2eb6a19d2545/cluster_marker_genes.h5ad --i ed8a4,7fd04 --annotations /home/jainj/hubmap/annotations.txt --o /home/jainj/hubmap/out  
+# inside downloaded dir...
+# ./prune.R --files $(pwd)/data/ed8a4dbbb1554a5e3227d6dfb2368828.h5ad,$(pwd)/data/7fd04d1aba61c35843dd2eb6a19d2545.h5ad --i ed8a4,7fd04 --annotations $(pwd)/misc/annotations.txt --o $(pwd)/output  
+
+library(docopt)
+library(Seurat)
+library(SeuratDisk)
+library(Matrix)
+library(cli)
+
+doc <- 
+  "Annotate and prune integrated HuBMAP object.
+
+Usage:
+  prune.R [--files <h5ad-files> --i <ids> --annotations <path-to-annotation-file> --o <output-directory> --verbose <verbosity> --threshold <threshold> --nsample <nsample>]
+
+Options:
+  --files <h5ad-files>                        Absolute paths to h5ad files to be integrated (unspaced, comma-separated).
+  --i <ids>                                   ID names of objects in h5ad files (unspaced, comma-separated).
+  --annotations <path-to-annotation-file>     Absolute path to file with cell type markers.
+  --o <output-directory>                      Output directory.
+  --threshold <threshold>                     Threshold for pruning [default: 0.75].
+  --nsample <nsample>                         Number of subsamples for pre- and post-pruned objects [default: 3].
+  --verbose <verbosity>                       Verbosity [default: TRUE]."
+
+opts <- docopt(doc)
+files <- unlist(strsplit(opts$files,","))
+ids <- unlist(strsplit(opts$i,","))
+annotations <- opts$annotations
+out <- opts$o
+v <- as.logical(opts$verbose)
+threshold <- as.numeric(opts$threshold)
+num.subsamples <- as.numeric(opts$nsample)
+
+cli_h1("Loading objects")
+integrated.all <- LoadH5Seurat(file = paste0(out,"/integrated.h5seurat"),verbose=v)
+objs <- list()
+for (i in 1:length(ids)) {
+  objs[[i]] <- LoadH5Seurat(file=paste0(out,"/",ids[[i]],".h5seurat"),verbose=v)
+}
+
+############  ANNOTATE ############  
+cli_h1("Adding annotations")
+first = T
+resolution = NULL
+cluster = NULL
+for (r in readLines(annotations)) { 
+  if (length(r)==0) {next}
+  if (any(grep(pattern = "RESOLUTION",x=r))) {
+    cluster = NULL
+    if (first) {
+      store <- as.vector(Idents(integrated.all))
+      first = F
+    } else {
+      store <- cbind(store,as.vector(Idents(integrated.all)))
+    }
+    
+    resolution = unlist(strsplit(x=r, split=" "))[2]
+    Idents(integrated.all) <- paste0('integrated_snn_res.',resolution)
+    next
+  }
+  if (is.null(cluster)) {
+    cluster = r 
+  } else {
+    names <- unlist(strsplit(x=r,split=","))
+    lst <- as.list(rep(cluster,length(names)))
+    names(lst) <- names
+    integrated.all <- RenameIdents(integrated.all, lst)
+    cluster = NULL
+  }
+}
+store <- cbind(store,as.vector(Idents(integrated.all)))
+
+integrated.all <- 
+  AddMetaData(
+    integrated.all,
+    col.name = "my.labels",
+    metadata = apply(X = store,
+                     MARGIN = 1,
+                     FUN = function(row) {
+                       row <- row[is.na(as.numeric(row))] # all non-numbers
+                       if (length(row) == 0) {return("Unknown")}
+                       return(row[length(row)]) # prioritizes highest resolution ID (rightmost column)
+                     }))
+Idents(integrated.all) <- 'my.labels'
+SaveH5Seurat(integrated.all, filename = paste0(out,"/integrated.h5seurat"),overwrite = T,verbose=v)
+
+############ SUBSAMPLE AND PRUNE ############  
+cli_h1("Subsampling to obtain transfer scores (pre-pruning)")
+anchors.subsampling <- list()
+predictions <- list()
+integrated.all <- AddMetaData(integrated.all,
+                              col.name='rand.sample',
+                              metadata=sample(1:num.subsamples,size=length(Cells(integrated.all)),replace=T))
+for (i in 1:num.subsamples) {
+  sub <- subset(integrated.all,subset=(rand.sample!=i))
+  query <- subset(integrated.all,subset=(rand.sample==i))
+  DefaultAssay(sub) <- "integrated"
+  DefaultAssay(query) <- "integrated"
+  sub[['SCT']] <- NULL
+  query[['SCT']] <- NULL
+  anchors.subsampling[[i]] <- FindTransferAnchors(reference = sub, query = query, npcs=NULL, k.filter=NA)
+  predictions[[i]] <- TransferData(anchorset = anchors.subsampling[[i]], refdata = Idents(sub))
+}
+
+# collect scores/labels from the sub-transfers
+all.scores <- rep(0,length(Cells(integrated.all)))
+names(all.scores) <- Cells(integrated.all)
+all.labels <- rep(0,length(Cells(integrated.all)))
+names(all.labels) <- Cells(integrated.all)
+for (i in 1:length(predictions)) {
+  sub <- subset(integrated.all,subset=(rand.sample==i))@meta.data
+  cells <- rownames(sub)
+  all.scores[cells] <- predictions[[i]]$prediction.score.max
+  all.labels[cells] <- predictions[[i]]$predicted.id
+}
+integrated.all <- AddMetaData(integrated.all,col.name='subsampling.score',
+                              metadata=all.scores)
+integrated.all <- AddMetaData(integrated.all,col.name='subsampling.label',
+                              metadata=all.labels)
+
+cli_h1("Pruning based on transfer scores")
+# prune out cells with subsampling.score < threshold (default 0.75)
+bad.cells <- Cells(integrated.all)[which(integrated.all[['subsampling.score',drop=T]]< threshold)]
+integrated.pruned <- subset(integrated.all,
+                            cells=Cells(integrated.all)[!Cells(integrated.all) %in% bad.cells])
+
+cli_h1("Re-processing pruned reference")
+# re-process pruned
+DefaultAssay(integrated.pruned) <- 'integrated'
+integrated.pruned <- RunPCA(integrated.pruned)
+integrated.pruned <- RunUMAP(integrated.pruned,dims=1:50)
+
+############  TRANSFER ############  
+cli_h1("Subsampling to obtain transfer scores + labels (post-pruning)")
+anchors.subsampling.postprune <- list()
+predictions <- list()
+integrated.pruned <- AddMetaData(integrated.pruned,
+                                 col.name='rand.sample',
+                                 metadata=sample(1:num.subsamples,size=length(Cells(integrated.pruned)),replace=T))
+for (i in 1:num.subsamples) {
+  sub <- subset(integrated.pruned,subset=(rand.sample!=i))
+  query <- subset(integrated.pruned,subset=(rand.sample==i))
+  DefaultAssay(sub) <- "integrated"
+  DefaultAssay(query) <- "integrated"
+  sub[['SCT']] <- NULL
+  query[['SCT']] <- NULL
+  anchors.subsampling.postprune[[i]] <- FindTransferAnchors(reference = sub, query = query, npcs=NULL, k.filter=NA)
+  predictions[[i]] <- TransferData(anchorset = anchors.subsampling.postprune[[i]], refdata = Idents(sub))
+}
+
+# get transferred labels for the pruned out cells as well
+i <- i + 1
+{
+  sub <- integrated.pruned
+  query <- subset(integrated.all,cells=Cells(integrated.all)[which(!(Cells(integrated.all) %in% Cells(integrated.pruned)))])
+  DefaultAssay(sub) <- "integrated"
+  DefaultAssay(query) <- "integrated"
+  sub[['SCT']] <- NULL
+  query[['SCT']] <- NULL
+  anchors.subsampling.postprune[[i]] <- FindTransferAnchors(reference = sub, query = query, npcs=NULL, k.filter=NA)
+  predictions[[i]] <- TransferData(anchorset = anchors.subsampling.postprune[[i]], refdata = Idents(sub))
+}
+
+# make sure you have all cells
+should.have <- NULL
+for (i in 1:length(objs)) {
+  should.have <- c(should.have,paste0(Cells(objs[[i]]),"_",i))
+}
+have <- NULL
+for (i in 1:length(predictions)) {
+  have <- c(have,rownames(predictions[[i]]))
+}
+stopifnot(setdiff(should.have,have)==0)
+stopifnot(setdiff(have,should.have)==0)
+
+# rbind the predictions metadata, and add to integrated.pruned object 
+predictions.all <- Reduce(rbind,predictions)
+names(predictions.all) <- paste0(names(predictions.all),".subsampling")
+names(predictions.all)[which(names(predictions.all)=='predicted.id.subsampling')] <-
+  'subsampling.label.postprune'
+names(predictions.all)[which(names(predictions.all)=='prediction.score.max.subsampling')] <-
+  'subsampling.score.postprune'
+labs <- predictions.all[Cells(integrated.pruned),]$'subsampling.label.postprune'
+scores <- predictions.all[Cells(integrated.pruned),]$'subsampling.score.postprune'
+integrated.pruned <- AddMetaData(integrated.pruned,col.name='subsampling.label.postprune',metadata=labs)
+integrated.pruned <- AddMetaData(integrated.pruned,col.name='subsampling.score.postprune',metadata=scores)
+
+# also add these to the original objs
+for (i in 1:length(objs)) {
+  df <- predictions.all[paste0(Cells(objs[[i]]),"_",i), ]
+  rownames(df) <- sapply(rownames(df),function(x) {return(Seurat:::ExtractField(x,field=1,delim="_"))})
+  objs[[i]] <- AddMetaData(objs[[i]],metadata=df)
+  Idents(objs[[i]]) <- 'subsampling.label.postprune'
+}
+
+cli_h1("Saving objects")
+SaveH5Seurat(integrated.all, filename = paste0(out,"/integrated.h5seurat"), overwrite=T, verbose=v)
+SaveH5Seurat(integrated.pruned, filename = paste0(out,"/integrated_pruned.h5seurat"), overwrite=T, verbose=v)
+for (i in 1:length(ids)) {
+  SaveH5Seurat(objs[[i]], filename=paste0(out,"/",ids[[i]],".h5seurat"),overwrite=T, verbose=v)
+}
+
